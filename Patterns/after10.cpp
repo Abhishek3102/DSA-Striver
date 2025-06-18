d6b634b21154f1ff71e47a934c8bd3dc32e2805f
Abhishek3102 <ankushpan694@gmail.com>
2025-06-18 12:20:52 +0530

    done

--- Patterns/after10.cpp
+++ Patterns/after10.cpp
@@ -231,15 +231,193 @@ ABCDCBA
 
 void print18(int n)
 {
-
-    for (int i = 0; i <= n; i++)
-    {
-        // char ch = 'E' - i;
-        // for (int j = i; j >= i; j--)
-        for (char ch = 'E' - i; ch <= 'E'; ch++)
+    /*
+    solution:*
+    4
+    E
+    D E
+    C D E
+    B C D E
+    A B C D E
+    /
+        for (int i = 0; i <= n; i++)
         {
-            cout << ch << " ";
-            // ch--;
+            // char ch = 'E' - i;
+            // for (int j = i; j >= i; j--)
+            for (char ch = 'E' - i; ch <= 'E'; ch++)
+            {
+                cout << ch << " ";
+                // ch--;
+            }
+            cout << endl;
+        }
+    }
+
+    void print19(int n)
+    {
+        /*
+        Whole solution :
+        5
+    **********
+    ****  ****
+    ***    ***
+    **      **
+    *        *
+    *        *
+    **      **
+    ***    ***
+    ****  ****
+    **********
+        */
+    int initialSpace = 0;
+    for (int i = 0; i < n; i++)
+    {
+        /*
+    this section :
+        5
+    **********
+    ****  ****
+    ***    ***
+    **      **
+    *        *
+
+    */
+        // stars
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "*";
+        }
+
+        // spaces
+        for (int j = 0; j < initialSpace; j++)
+        {
+            cout << " ";
+        }
+
+        // stars
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "*";
+        }
+        initialSpace += 2;
+        cout << endl;
+    }
+    initialSpace = 2 * n - 2;
+    for (int i = 1; i <= n; i++)
+    {
+        /*
+    this section :
+        5
+    *        *
+    **      **
+    ***    ***
+    ****  ****
+    **********
+
+    */
+        // stars
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+
+        // spaces
+        for (int j = 0; j < initialSpace; j++)
+        {
+            cout << " ";
+        }
+
+        // stars
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        initialSpace -= 2;
+        cout << endl;
+    }
+}
+
+void print20(int n)
+{
+    /*
+    solution :
+    5
+    *       *
+    **     **
+    ***   ***
+    **** ****
+    **********
+    **** ****
+    ***   ***
+    **     **
+    *       *
+    */
+    int spaces = 2 * n - 2;
+    for (int i = 1; i <= 2 * n - 1; i++)
+    {
+        int stars = i;
+        if (i > n)
+            stars = 2 * n - i;
+        // stars
+        for (int j = 1; j <= stars; j++)
+        {
+            cout << "*";
+        }
+
+        // spaces
+        for (int j = 1; j < spaces; j++)
+        {
+            cout << " ";
+        }
+        // stars
+        for (int j = 1; j <= stars; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+        if (i < n)
+            spaces -= 2;
+        else
+            spaces += 2;
+    }
+}
+
+void print21(int n)
+{
+    /*
+    solution :
+    4
+    ****
+    *  *
+    *  *
+    ****
+    */
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == 0 || j == 0 || i == n - 1 || j == n - 1)
+            {
+                cout << "*";
+            }
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+void print22(int n)
+{
+    for (int i = 0; i < 2 * n - 1; i++)
+    {
+        for (int j = 0; j < 2 * n - 1; j++)
+        {
+            int top = i;
+            int left = j;
+            int right = (2 * n - 2) - j;
+            int down = (2 * n - 2) - i;
+            cout << (n - min(min(top, down), min(left, right)));
         }
         cout << endl;
     }
@@ -253,6 +431,6 @@ int main()
     {
         int n;
         cin >> n;
-        print18(n);
+        print22(n);
     }
 }
