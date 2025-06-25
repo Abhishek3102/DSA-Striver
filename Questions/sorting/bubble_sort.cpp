fc38a22edf98d84621faef32466dfbb6e247d4a6
Abhishek3102 <ankushpan694@gmail.com>
2025-06-25 22:26:35 +0530

    implement selection and bubble sort

---
+++ Questions/sorting/bubble_sort.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubb_sort(int arr[], int n)
+{
+    for (int i = n - 1; i >= 1; i--)
+    {
+        for (int j = 0; j < i - 1; j++) // here j is going till i-1 bcoz if we went till i instead of i-1
+        // then last element would have no next element to compare itself with
+        // and it would have given a runtime error bcoz we are trying to access an index which is not present
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    bubb_sort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
