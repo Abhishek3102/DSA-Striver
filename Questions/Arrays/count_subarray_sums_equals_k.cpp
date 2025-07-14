2f7cb7402af02a0f1c138ef36a3c9bd09b95a40b
Abhishek3102 <ankushpan694@gmail.com>
2025-07-15 01:10:45 +0530

    added optimal solution of find count of subarray with sum k(Added function `findAllSubarraysWithGivenSumOptimal` to efficiently find all subarrays whose sum equals a given target `k` by utilizing a hash map to store the frequency of prefix sums, reducing time complexity to O(n), and tracking the difference between the current prefix sum and the target sum to ensure accurate subarray count.)

--- Questions/Arrays/count_subarray_sums_equals_k.cpp
+++ Questions/Arrays/count_subarray_sums_equals_k.cpp
@@ -82,6 +82,42 @@ int find_num_subarray_Sum_k_better(int arr[], int n, int target)
     return count;
 }
 
+int findAllSubarraysWithGivenSumOptimal(vector<int> &arr, int k)
+{
+    // Step 1: Declare a map to store the frequency of prefix sums
+    map<int, int> mpp;
+
+    // Step 2: Initialize the map with {0: 1}.
+    // This is because a prefix sum of 0 means a valid subarray can start from index 0 itself if the sum equals `k`.
+    mpp[0] = 1;
+
+    // Step 3: Initialize variables for prefix sum and the result count
+    int preSum = 0, count = 0;
+
+    // Step 4: Iterate through the array to calculate prefix sums and count subarrays with sum `k`
+    for (int i = 0; i < arr.size(); i++)
+    {
+
+        // Step 5: Add the current element to the prefix sum
+        preSum += arr[i];
+
+        // Step 6: Calculate the difference (remove) between current prefix sum and target sum `k`
+        // This represents the sum that should have existed earlier in the array for the subarray sum to be `k`
+        int remove = preSum - k;
+
+        // Step 7: If this difference (remove) exists in the map, it means we've seen a prefix sum before
+        // that, when subtracted from the current prefix sum, gives the target sum `k`
+        count += mpp[remove];
+
+        // Step 8: Update the map with the current prefix sum.
+        // It helps to count how many times we've seen this prefix sum
+        mpp[preSum] += 1;
+    }
+
+    // Step 9: Return the final count of subarrays with sum `k`
+    return count;
+}
+
 int main()
 {
     int arr[] = {1, 5, -2, 4, 1, 2, 1, 1, 1};
