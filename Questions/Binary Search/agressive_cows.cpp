b36a30aaa6e1f83ad33650dfad771a1e2ef1067c
Abhishek3102 <ankushpan694@gmail.com>
2025-08-25 22:38:04 +0530

    mplemented the Aggressive Cows problem using binary search to find the largest minimum distance for placing cows in stalls. Added helper function canWePlace to validate cow placement feasibility for a given distance.

---
+++ Questions/Binary Search/agressive_cows.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool canWePlace(vector<int> &stalls, int distance, int cows)
+{
+    int n = stalls.size();
+    int cntCows = 1, last = stalls[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (stalls[i] - last >= distance)
+        {
+            cntCows++;
+            last = stalls[i];
+        }
+        if (cntCows >= cows)
+            return true;
+    }
+    return false;
+}
+
+int agressiveCows(vector<int> &stalls, int k)
+{
+    sort(stalls.begin(), stalls.end());
+    int n = stalls.size();
+    int low = 1, high = stalls[n - 1] - stalls[0];
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (canWePlace(stalls, mid, k) == true)
+        {
+            low = mid + 1;
+        }
+
+        else
+        {
+            high = mid - 1;
+        }
+    }
+
+    return high;
+}
