3ed3e91bdb035d754c1740a94106157150ed8c9c
Abhishek3102 <ankushpan694@gmail.com>
2026-06-20 03:04:53 +0530

    feat: implement painter partitioning algorithm with detailed comments and logic for workload distribution

--- Questions/Binary Search/painterPartitionOrSplitArrayLargest.cpp
+++ Questions/Binary Search/painterPartitionOrSplitArrayLargest.cpp
@@ -12,9 +12,131 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int countPainter(vector<int> &arr, int) {}
+/*
+    Given a maximum amount of work (k),
+    determine how many painters are required.
+
+    Each painter can only paint a consecutive
+    sequence of boards (subarray).
+
+    We greedily assign boards to the current painter
+    until adding another board would exceed k.
+    Then we allocate a new painter.
+*/
+int countPainter(vector<int> &arr, int k)
+{
+    int n = arr.size();
+
+    // At least one painter is always needed.
+    int painters = 1;
+
+    // Current workload assigned to the current painter.
+    long long colors = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        // If current board can be assigned to the
+        // current painter without exceeding limit k.
+        if (colors + arr[i] <= k)
+        {
+            colors += arr[i];
+        }
+        else
+        {
+            // Need a new painter.
+            painters++;
+
+            // Start the new painter with current board.
+            colors = arr[i];
+        }
+    }
+
+    return painters;
+}
+
+/*
+    Find the minimum possible maximum workload
+    among all painters.
+
+    l = number of boards
+    m = number of painters
+
+    Binary Search Range:
+    --------------------
+    low  = largest board length
+           (because some painter must paint it)
+
+    high = sum of all board lengths
+           (one painter paints everything)
+
+    We binary search for the smallest feasible
+    maximum workload.
+*/
+int findPainters(vector<int> &arr, int l, int m)
+{
+    // More painters than boards is invalid because
+    // every painter must paint at least one board.
+    if (m > l)
+        return -1;
+
+    // Minimum possible answer.
+    int low = *max_element(arr.begin(), arr.end());
+
+    // Maximum possible answer.
+    int high = accumulate(arr.begin(), arr.end(), 0);
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        // Number of painters required if no painter
+        // is allowed to paint more than 'mid' work.
+        int painters = countPainter(arr, mid);
+
+        if (painters > m)
+        {
+            /*
+                Too many painters are needed.
+
+                This means the workload limit (mid)
+                is too small, so increase it.
+            */
+            low = mid + 1;
+        }
+        else
+        {
+            /*
+                We can paint everything using
+                <= m painters.
+
+                Try to reduce the maximum workload
+                further and search on left side.
+            */
+            high = mid - 1;
+        }
+    }
+
+    /*
+        low will point to the smallest feasible
+        maximum workload.
+    */
+    return low;
+}
+
+/*
+    Wrapper function.
+*/
+int findLargestMinAllocation(vector<int> &houses, int x)
+{
+    return findPainters(houses, houses.size(), x);
+}
 
 int main()
 {
+    vector<int> arr = {10, 20, 30, 40};
+    int painters = 2;
+
+    cout << findLargestMinAllocation(arr, painters);
+
     return 0;
 }
